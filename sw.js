56f2d4a1e0451d7c453f0afd33af2046c70012c5
sebastian ramirez <sr2409812@gmail.com>
2026-09-22 08:20:14 -0600

    Bump cache v18 por seguridad de nombres PDF

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cnfl-campo-v17';
+const CACHE_NAME = 'cnfl-campo-v18';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -16,7 +16,7 @@ const ASSETS_TO_CACHE = [
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('[SW v17] Pre-caching assets offline...');
+      console.log('[SW v18] Pre-caching assets offline...');
 
       return cache.addAll(ASSETS_TO_CACHE).catch(err => console.warn('[SW] Cache addAll warning:', err));
     }).then(() => self.skipWaiting())
