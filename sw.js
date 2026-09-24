a945e0a549bb866a09d687e921bea9fd6a44dfe1
sebastian ramirez <sr2409812@gmail.com>
2026-09-24 07:43:59 -0600

    Bump cache v22 Telegram panel

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cnfl-campo-v21';
+const CACHE_NAME = 'cnfl-campo-v22';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -16,7 +16,7 @@ const ASSETS_TO_CACHE = [
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('[SW v21] Pre-caching assets offline...');
+      console.log('[SW v22] Pre-caching assets offline...');
 
       return cache.addAll(ASSETS_TO_CACHE).catch(err => console.warn('[SW] Cache addAll warning:', err));
     }).then(() => self.skipWaiting())
